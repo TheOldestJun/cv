ac992fcd573f54794f6e016ac29157f9e2cf8160
MomFriendsSon <s.v.pukha@gmail.com>
2021-12-17 14:41:02 +0200

    Education start

--- src/components/educations.component.js
+++ src/components/educations.component.js
@@ -2,6 +2,20 @@ import React, { Component } from "react";
 
 export default class Educations extends Component {
   render() {
-    return <h2>Educations</h2>;
+    return (
+      <div>
+        <div className="card brown lighten-3 z-depth-0">
+          <div className="card-content">
+            <h6 className="brown-text text-darken-4">
+              <strong>
+                <i className="fas fa-graduation-cap"></i>
+                EDUCATION
+              </strong>
+            </h6>
+            <hr />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
